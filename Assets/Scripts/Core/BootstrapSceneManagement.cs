d53fc582a3ba510d9401302f299b5a1430572b3f
Roman Dunin <abromus@mail.ru>
2023-04-03 20:52:47 +0300

    Запуск с любой сцены в редакторе

---
+++ Assets/Scripts/Core/BootstrapSceneManagement.cs
@@ -0,0 +1,28 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace Asteroids.Core
+{
+    [InitializeOnLoad]
+    public class BootstrapSceneManagement
+    {
+        static BootstrapSceneManagement()
+        {
+            OnSceneListChanged();
+            EditorBuildSettings.sceneListChanged += OnSceneListChanged;
+        }
+
+        private static void OnSceneListChanged()
+        {
+            if (EditorBuildSettings.scenes.Length == 0)
+                return;
+
+            var startSceneIndex = 0;
+            var scene = AssetDatabase.LoadAssetAtPath<SceneAsset>(EditorBuildSettings.scenes[startSceneIndex].path);
+
+            EditorSceneManager.playModeStartScene = scene;
+        }
+    }
+}
+#endif
