f69d5385c4974f099439c3d101c50897c4f31d52
Roman Dunin <abromus@mail.ru>
2023-04-12 19:14:32 +0300

    Реализованы реактивные свойства

---
+++ Assets/Scripts/Core/Common/IReactiveProperty.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Asteroids.Core
+{
+    public interface IReactiveProperty<T>
+    {
+        public T Value { get; set; }
+
+        public Action<T> OnChanged { get; set; }
+    }
+}
---
+++ Assets/Scripts/Core/Common/ReactiveProperty.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Asteroids.Core
+{
+    public class ReactiveProperty<T> : IReactiveProperty<T>
+    {
+        private T _value;
+
+        public T Value
+        {
+            get
+            {
+                return _value;
+            }
+
+            set
+            {
+                _value = value;
+
+                OnChanged.SafeInvoke(_value);
+            }
+        }
+
+        public Action<T> OnChanged { get; set; }
+    }
+}
